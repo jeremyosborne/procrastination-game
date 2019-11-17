04c656b6473f7ab529ab2b49332b09d21da76802
Jeremy Osborne <jeremywosborne@gmail.com>
2019-11-17 11:12:38 -0800

    lions and tigers and webpack, oh my
    Also, jot some thoughts down about tool usage. Tool usage has become inescable in web projects, and I'd like to stew on my opinions of the current tools while futzing about here. If it works, will copy docs to boilerplate repo.

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 Silly, unlockable content for my webpage, a reward for those who loiter.
 
+## Environment configuration
+
+* `NODE_PATH=src/apps:src` - applications will namespace due to allow for common names across different applications, root level modules are considered common.
+* `BUILD_TYPE=development` - valid values {`development` | `production`}, defaults to `production`.
+    * Editorial: avoid the nuanced behavior and occasional conflicts between libs caused by setting NODE_ENV and the magic of webpack env. `development` means debug(gable). `production` means optimized, and if not optimal, it is a bug. However, we do appropriate the vernacular because they map well to other libs that adopt them with the semantic meanings defined above.
+
+## Toolchain notes
+
+Modern JS needs a tool chain and build process. The tools included in this project should stick to their job and be runnable as a distinct process as much as possible.
+
+* `package.json` scripts define separate build names, which within the scripts will define build time variables.
+* `webpack.config.js` consumes build time variables and translates them into a suitable `webpack` configuration.
+* `webpack` acts as build orchestrator, linker, and packager. We use just enough of its magic to keep ourselves productive.
+* `postcss` tools have the job of CSS compiler since, to this day, Douglas Crawford is right about the meaning of `C` in `CSS`. Plus it also allows us to think of and pack our components as modules and separate out global CSS applied to all pages of our application from module specific CSS that should be managed by webpack.
+* `babel` has the job of JS code compiler and it's configuration should be good enough to take source code and turn it into intermediate style code that is handed off to other steps in the build process.
+
 ## Requirements
 
 * Internals
--- webpack.config.js
+++ webpack.config.js
@@ -2,77 +2,107 @@ const CopyPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
-// TODO: need a production build and some notes about file system structure
-// since this is going to become my blog.
+// Have more `BUILD_TYPE`s? Add them here. The config must also be made to support them.
+const SUPPORTED_BUILD_TYPES = ['development', 'production'].reduce((types, t) => {
+  types[t] = t
+  return types
+}, {})
+const SUPPORTED_BUILD_TYPES_DEFAULT = 'production'
 
-module.exports = {
-  devServer: {
-    // The application may request static, non compiled assets. If these exist
-    // they will be located in this directory. There seems to be multiple solutions
-    // to this, like with the copy-webpack-plugin, but this seems to be the best
-    // solution for the dev-server "build" since it prevents unnecessary copying.
-    contentBase: path.join(__dirname, 'static'),
-    hot: true,
-  },
-  devtool: 'inline-source-map',
-  entry: path.join(__dirname, 'src/apps/achievements/index.js'),
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
+module.exports = (env, argv) => {
+  //
+  // debug or optimized builds?
+  //
+  let buildType = SUPPORTED_BUILD_TYPES[process.env.BUILD_TYPE]
+  if (process.env.BUILD_TYPE && !buildType) {
+    throw new Error(`Passed unknown BUILD_TYPE of ${process.env.BUILD_TYPE}. Please use one of the supported types of: ${Object.keys(SUPPORTED_BUILD_TYPES.join(', '))}.`)
+  } else if (!buildType) {
+    // not passing a build type is fine.
+    buildType = SUPPORTED_BUILD_TYPES_DEFAULT
+  }
+  console.log('Using buildType:', buildType)
+
+  // Prevent cwd problems by anchoring everything from the location of the webpack file.
+  const ROOT_DIR = path.resolve(__dirname)
+
+  return {
+    devServer: {
+      // The application may request static, non compiled assets. If these exist
+      // they will be located in this directory. There seems to be multiple solutions
+      // to this, like with the copy-webpack-plugin, but this seems to be the best
+      // solution for the dev-server "build" since it prevents unnecessary copying.
+      contentBase: path.join(__dirname, 'static'),
+      hot: true,
+    },
+    devtool: 'eval-source-map', // static output: source-map
+    entry: path.join(ROOT_DIR, 'src/apps/achievements/index.js'),
+    // For now, let webpack do a lot of the work for us since our buildTypes match theirs.
+    mode: buildType,
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+              },
             },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              // Due to https://github.com/postcss/postcss-loader/issues/204#issuecomment-380885660
-              // don't use external config, it seems duplicate work.
-              plugins: [
-                require('autoprefixer'),
-              ]
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                // Due to https://github.com/postcss/postcss-loader/issues/204#issuecomment-380885660
+                // don't use external config, it seems duplicate work.
+                plugins: [
+                  require('autoprefixer'),
+                ]
+              }
             }
-          }
-        ],
-      },
-      {
-        test: /\.jsx?$/i,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
+          ],
         },
-      },
+        {
+          test: /\.jsx?$/i,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+    output: {
+      filename: 'achievements-main.js',
+      path: path.resolve(ROOT_DIR, 'dist'),
+    },
+    plugins: [
+      new CopyPlugin([
+        {
+          from: path.join(ROOT_DIR, 'static'),
+          to: path.join(ROOT_DIR, 'dist')
+        },
+      ]),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: path.join(ROOT_DIR, 'src/assets/index.html'),
+      }),
     ],
-  },
-  output: {
-    filename: 'achievements-main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new CopyPlugin([
-      {from: path.join(__dirname, 'static'), to: path.join(__dirname, 'dist')},
-    ]),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.join(__dirname, 'src/assets/index.html'),
-    }),
-  ],
-  resolve: {
-    // options for resolving module requests
-    // (does not apply to resolving to loaders)
-    modules: [
-      'node_modules',
-      path.resolve(__dirname, 'src/apps'),
-      path.resolve(__dirname, 'src'),
-    ],
-    // directories where to look for modules
-    extensions: ['.js', '.json', '.jsx', '.css'],
-  },
+    resolve: {
+      // options for resolving module requests
+      // (does not apply to resolving to loaders)
+      modules: [
+        'node_modules',
+        path.resolve(ROOT_DIR, 'src/apps'),
+        path.resolve(ROOT_DIR, 'src'),
+      ],
+      // directories where to look for modules
+      extensions: [
+        '.js',
+        '.json',
+        '.jsx',
+        '.css',
+      ],
+    },
+  }
 }
